b4b10ccc1ebe705f0d9a804699c2e042b7924712
shubhammane21 <shubhammane21@hotmail.com>
2023-12-11 03:14:00 +0530

    Updates

---
+++ precache-manifest.58df13403c3155630d303ad5cbb6f116.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5226331ff6d7a546eaa0b8331fd5127f",
+    "url": "/WeatherApp/index.html"
+  },
+  {
+    "revision": "6bc80bd24bd10f4e28eb",
+    "url": "/WeatherApp/static/css/main.783e3c12.chunk.css"
+  },
+  {
+    "revision": "ef6dc62a36024946398f",
+    "url": "/WeatherApp/static/js/2.ba633ce8.chunk.js"
+  },
+  {
+    "revision": "65a9926ecde5afbe3bda",
+    "url": "/WeatherApp/static/js/3.3b6fb9ae.chunk.js"
+  },
+  {
+    "revision": "6bc80bd24bd10f4e28eb",
+    "url": "/WeatherApp/static/js/main.d66b2c17.chunk.js"
+  },
+  {
+    "revision": "4420a8a7833a91c1709c",
+    "url": "/WeatherApp/static/js/runtime~main.1d8b57cd.js"
+  }
+]);

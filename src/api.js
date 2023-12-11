a066ce87ef24b86e7c68af2dff26e9229d8ce584
shubhammane21 <shubhammane21@hotmail.com>
2023-12-11 13:31:26 +0530

    first commit

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
  export async function fetchWeather(city,setError){
     console.log(process.env.REAC_APP_API_KEY);
-    const url =`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=07848c80e5e86e803fb1770303bfa3c2`;
+    const url =`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid={api-id}`;
     try {
         const response =await axios.get(url); 
         setError("");
